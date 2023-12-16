ba3f84570baf20f4a5c9e39a7908d9238bb16228
Hannah Ahn <ahnlook90@gmail.com>
2023-12-16 16:55:13 +0900

    refactor: Refactor for ESLint compliance

--- src/App.js
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div className='App'>Rainbow Letter!</div>
-}
-
-export default App
---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function App() {
+  return <div className="App">Rainbow Letter!</div>;
+}
+
+export default App;
