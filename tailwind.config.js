b29ab2593fb662b31673960ac77d3df90e81aaa1
Hannah Ahn <ahnlook90@gmail.com>
2023-12-17 14:49:08 +0900

    chore: tailwindcss 설정 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: ['./src/**/*.{html,js,jsx}'],
   theme: {
     fontFamily: { sans: ['Noto Sans KR', 'sans-serif'] },
     extend: {
