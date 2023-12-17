f6a2b9fc589df4518b11c0cd28f377992d06df9c
Hannah Ahn <ahnlook90@gmail.com>
2023-12-17 19:54:50 +0900

    fix: tailwindcss 설정 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,54 +5,28 @@ module.exports = {
     fontFamily: { sans: ['Noto Sans KR', 'sans-serif'] },
     extend: {
       fontSize: {
-        heading: {
-          1: {
-            pc: ['3rem', { lineHeight: '130%', fontWeight: '600' }],
-            default: ['1.875rem', { lineHeight: '130%', fontWeight: '600' }],
-          },
-          2: {
-            pc: ['2.25rem', { lineHeight: '130%', fontWeight: '500' }],
-            default: ['1.625rem', { lineHeight: '130%', fontWeight: '500' }],
-          },
-          3: {
-            pc: ['1.5rem', { lineHeight: '130%', fontWeight: '500' }],
-            default: ['1.25rem', { lineHeight: '130%', fontWeight: '500' }],
-          },
-        },
-        solo: {
-          Large: ['1.125rem', { lineHeight: '100%', fontWeight: '500' }],
-          medium: {
-            pc: ['1rem', { lineHeight: '100%' }],
-            default: ['1.25rem', { lineHeight: '100%' }],
-          },
-          small: {
-            pc: ['0.875rem', { lineHeight: '100%' }],
-            default: ['1rem', { lineHeight: '100%' }],
-          },
-          label: {
-            pc: ['0.875rem', { lineHeight: '100%', fontWeight: '500' }],
-            default: ['1rem', { lineHeight: '100%', fontWeight: '500' }],
-          },
-        },
-        body: {
-          large: ['1.125rem', { lineHeight: '166%', fontWeight: '500' }],
-          medium: {
-            pc: ['1rem', { lineHeight: '166%' }],
-            default: ['1.25rem', { lineHeight: '166%' }],
-          },
-          small: {
-            pc: ['0.875rem', { lineHeight: '166%' }],
-            default: ['1rem', { lineHeight: '166%' }],
-          },
-          letter: {
-            pc: ['2.25rem', { lineHeight: '166%' }],
-            default: ['1.625rem', { lineHeight: '166%' }],
-          },
-        },
-        caption: {
-          pc: ['0.75rem', { lineHeight: '166%' }],
-          default: ['0.875rem', { lineHeight: '166%' }],
-        },
+        'heading-1': ['1.875rem', { lineHeight: '130%', fontWeight: '600' }],
+        'heading-1-pc': ['3rem', { lineHeight: '130%', fontWeight: '600' }],
+        'heading-2': ['130%', { lineHeight: '130%', fontWeight: '500' }],
+        'heading-2-pc': ['2.25rem', { lineHeight: '130%', fontWeight: '500' }],
+        'heading-3': ['1.25rem', { lineHeight: '130%', fontWeight: '500' }],
+        'heading-3-pc': ['1.5rem', { lineHeight: '130%', fontWeight: '500' }],
+        'solo-large': ['1.125rem', { lineHeight: '100%', fontWeight: '500' }],
+        'solo-medium': ['1rem', { lineHeight: '100%' }],
+        'solo-medium-pc': ['1rem', { lineHeight: '100%' }],
+        'solo-small': ['0.875rem', { lineHeight: '100%' }],
+        'solo-small-pc': ['0.875rem', { lineHeight: '100%' }],
+        'solo-label': ['0.875rem', { lineHeight: '100%', fontWeight: '500' }],
+        'solo-label-pc': ['1rem', { lineHeight: '100%', fontWeight: '500' }],
+        'body-large': ['1.125rem', { lineHeight: '166%', fontWeight: '500' }],
+        'body-medium': ['1rem', { lineHeight: '166%' }],
+        'body-medium-pc': ['1.25rem', { lineHeight: '166%' }],
+        'body-small': ['0.875rem', { lineHeight: '166%' }],
+        'body-small-pc': ['1rem', { lineHeight: '166%' }],
+        'body-letter': ['1.625rem', { lineHeight: '166%' }],
+        'body-letter-pc': ['2.25rem', { lineHeight: '166%' }],
+        caption: ['0.875rem', { lineHeight: '166%' }],
+        'caption-pc': ['0.75rem', { lineHeight: '166%' }],
       },
       colors: {
         primary: '#FFB347',
@@ -71,9 +45,7 @@ module.exports = {
         },
       },
       textColor: {
-        heading: {
-          black: '#212121',
-        },
+        default: '#212121',
         gray: {
           1: '#585667',
           2: '#BDBDBD',
