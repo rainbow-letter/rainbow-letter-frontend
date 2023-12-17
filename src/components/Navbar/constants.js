a665c2acff1ea48ed4107118ae0809ba3e7f92d0
Hannah Ahn <ahnlook90@gmail.com>
2023-12-17 18:17:03 +0900

    feat: navbar 생성

---
+++ src/components/Navbar/constants.js
@@ -0,0 +1,23 @@
+const navConfig = {
+  '/my-page': {
+    title: '마이 페이지',
+    actionName: '로그아웃',
+    action: () => {
+      // TODO: 로그아웃 기능 구현
+    },
+  },
+  '/write-letter': {
+    title: '편지쓰기',
+  },
+  '/letter-box': {
+    title: '편지함',
+  },
+  'my-pet': {
+    title: '내 반려동물',
+  },
+  '/my-page/register': {
+    title: '등록하기',
+  },
+};
+
+export default navConfig;
