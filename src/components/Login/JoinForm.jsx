ede7bc35646c33a265820e73d4e7f63505967ebb
Hyuk <poiu03290@gmail.com>
2023-12-18 17:39:13 +0900

    feat: 회원가입 fetch 초안 작성

--- src/components/Login/JoinForm.jsx
+++ src/components/Login/JoinForm.jsx
@@ -1,12 +1,30 @@
-import { React } from 'react';
+import { React, useCallback } from 'react';
 
 const INPUT_STYLE =
   'w-full rounded-[15px] py-[21.5px] pl-[21.25px] bg-gray-2 text-gray-2 text-solo-small mb-2.5';
 
-export default function LoginForm({
+export default function JoinForm({
   message: { describe, button },
   BUTTON_STYLE,
 }) {
+  const data = {
+    email: 'handwoong@gmail.com',
+    password: 'password1',
+  };
+
+  const onClickLoginButton = useCallback(async (e) => {
+    e.preventDefault();
+    const response = await fetch('http://52.79.240.249:8081/api/members', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const jsonData = await response.json();
+    console.log(jsonData);
+  }, []);
+
   return (
     <section>
       <h3 className="text-solo-small text-gray-1">{describe}</h3>
@@ -23,6 +41,7 @@ export default function LoginForm({
         />
         <button
           type="submit"
+          onClick={(e) => onClickLoginButton(e)}
           className={`${BUTTON_STYLE} bg-orange-400 text-heading-3 text-white mt-0.5`}
         >
           {button.default}
