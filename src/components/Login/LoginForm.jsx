e4e47a62d4b2772fa2ffb90b1b9ffab02d11d8aa
Hyuk <poiu03290@gmail.com>
2023-12-18 18:45:51 +0900

    feat: 로그인 api 구현(useState)

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -1,27 +1,34 @@
-import { React, useCallback } from 'react';
+import { React, useState, useCallback } from 'react';
 
 import { trylogin } from '../../api/user';
 
 const INPUT_STYLE =
   'w-full rounded-[15px] py-[21.5px] pl-[21.25px] bg-gray-2 text-gray-2 text-solo-small mb-2.5';
-const data = {
-  email: 'handwoong@gmail.com',
-  password: 'password1',
-};
+
 export default function LoginForm({
   message: { describe, button },
   BUTTON_STYLE,
 }) {
-  const onClickLoginButton = useCallback(async (e) => {
-    try {
-      e.preventDefault();
-      const response = await trylogin(data);
-      console.log(response);
-    } catch (error) {
-      const errorData = error.response.data;
-      console.log(errorData);
-    }
-  }, []);
+  const [profile, setProfile] = useState({
+    email: '',
+    password: '',
+  });
+
+  const onClickLoginButton = useCallback(
+    async (e) => {
+      try {
+        e.preventDefault();
+        const data = await trylogin(profile);
+
+        console.log(data);
+      } catch (error) {
+        const errorData = error.response.data;
+
+        console.log(errorData);
+      }
+    },
+    [profile]
+  );
 
   return (
     <section>
@@ -29,11 +36,15 @@ export default function LoginForm({
       <form className="my-5">
         <input
           type="text"
+          value={profile.email}
+          onChange={(e) => setProfile({ ...profile, email: e.target.value })}
           placeholder="이메일을 입력해주세요"
           className={`${INPUT_STYLE} mb`}
         />
         <input
           type="password"
+          value={profile.password}
+          onChange={(e) => setProfile({ ...profile, password: e.target.value })}
           placeholder="비밀번호를 입력해주세요"
           className={INPUT_STYLE}
         />
