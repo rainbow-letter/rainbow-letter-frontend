1e3f0086a858c1f643b3ad43964ecfecba937ce7
Hannah Ahn <ahnlook90@gmail.com>
2023-12-19 19:41:52 +0900

    style: layout 스타일 수정

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Layout({ className, children }) {
   return (
-    <div className="w-full h-screen min-w-[360px] flex justify-center">
-      <div className={`${className} w-[360px] bg-white px-6 pt-7`}>
+    <div className="w-full h-full min-w-[360px] pb-10 flex justify-center">
+      <div className={`${className} w-[390px] bg-white px-6 pt-7`}>
         {children}
       </div>
     </div>
