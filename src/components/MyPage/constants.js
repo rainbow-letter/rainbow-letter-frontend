777dd1f64582c9f7d8d1359e2fb774f78330e619
Hannah Ahn <ahnlook90@gmail.com>
2023-12-19 19:41:12 +0900

    feat: 마이페이지 상수 선언

--- src/components/MyPage/constants.js
+++ src/components/MyPage/constants.js
@@ -1,42 +1,49 @@
-export const titles = {
-  header: '마이페이지',
-  body: '내 정보',
-  footer: '자주 묻는 질문',
+export const PAGE_TITLES = {
+  NAVBAR: '마이페이지',
+  MY_INFO: '내 정보',
+  FAQ: '자주 묻는 질문',
 };
 
-export const headerContents = {
-  logOut: '로그아웃',
+export const USER_INFO_LABELS = {
+  EMAIL: '이메일',
+  PHONE: '휴대폰 번호',
+  NO_PHONE: '등록된 번호 없음',
+  CHANGE_PASSWORD: '비밀번호 변경하기',
 };
 
-export const userInfoContents = {
-  email: '이메일',
-  phone: '휴대폰 번호',
-  noPhone: '등록된 번호 없음',
-  edit: '수정하기',
+export const USER_INFO_MESSAGES = {
+  ENTER_DIGITS_ONLY: '-를 제외한 숫자만 넣어주세요',
+  INVALID_PHONE: '번호를 다시 확인해주세요',
 };
 
-export const FAQs = [
+export const USER_ACTIONS = {
+  LOG_OUT: '로그아웃',
+  EDIT: '수정하기',
+  FINISH: '확인',
+};
+
+export const FAQS = [
   {
-    id: 1,
-    title: '무지개편지는 어떤 서비스인가요?',
-    content:
+    ID: 1,
+    QUESTION: '무지개편지는 어떤 서비스인가요?',
+    ANSWER:
       '펫로스 증후군을 겪고 있는 분들을 편지 주고 받기를 통해 위로하는 서비스예요. 심리상담학에서는 ‘글쓰기는 치유의 과정’이라고 강조해요. 자기 감정을 마음껏 표현하며 충분한 추모의 시간을 보내시길 바래요.',
   },
   {
-    id: 2,
-    title: '답장은 언제 오나요?',
-    content:
-      '편지를 보내고 1~2일 이내 도착해요. 서비스 내 편지함에서 확인해주세요.',
+    ID: 2,
+    QUESTION: '답장은 언제 오나요?',
+    ANSWER:
+      '편지를 보내고 1~2일 이내 도착해요.\n서비스 내 편지함에서 확인해주세요.',
   },
   {
-    id: 3,
-    title: '답장이 온 건 어떻게 알 수 있나요?',
-    content:
+    ID: 3,
+    QUESTION: '답장이 온 건 어떻게 알 수 있나요?',
+    ANSWER:
       '답장이 도착하면 가입하신 이메일 주소로 메일을 보내드려요. 핸드폰번호를 등록하면 문자도 보내드려요.',
   },
   {
-    id: 4,
-    title: '탈퇴는 어떻게 하나요?',
-    content: '아래 이메일로 문의 주시면 도와드릴게요. swcteam41@gmail.com',
+    ID: 4,
+    QUESTION: '탈퇴는 어떻게 하나요?',
+    ANSWER: '아래 이메일로 문의 주시면 도와드릴게요.\nswcteam41@gmail.com',
   },
 ];
