735ac05566f32e9f3a27902205d4ac99137a5b45
Hyuk <poiu03290@gmail.com>
2023-12-21 02:32:26 +0900

    feat: 비밀번호 변경하기 페이지 구현

--- src/components/Login/constants.js
+++ src/components/Login/constants.js
@@ -31,6 +31,11 @@ const LOGIN_MESSAGE = [
   },
 ];
 
+const ERROR_MESSAGE = Object.freeze({
+  NOT_MATCH: '비밀번호를 다시 확인해주세요.',
+  NOT_VALID_FORM: '영어, 숫자 포함 8자리 이상 입력해주세요.',
+});
+
 const BUTTON_STYLE = 'w-full rounded-[15px] flex justify-center items-center';
 
-export { LOGIN_MESSAGE, BUTTON_STYLE };
+export { LOGIN_MESSAGE, BUTTON_STYLE, ERROR_MESSAGE };
--- src/view/Password.jsx
+++ src/view/Password.jsx
@@ -1,23 +1,27 @@
 /* eslint-disable */
-import { React, useEffect, useCallback } from 'react';
+import { React, useState, useEffect, useCallback } from 'react';
 import { useSearchParams, useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 
 import UserInput from '../components/Login/UserInput';
+import { validatePasswordMatch, validatePassword } from '../utils/validators';
 import { getToken } from '../store/user';
 import { updatePassword } from '../api/user';
 
-import { BUTTON_STYLE } from '../components/Login/constants';
-
-const data = {
-  password: null,
-  newPassword: 'user1234',
-};
+import { BUTTON_STYLE, ERROR_MESSAGE } from '../components/Login/constants';
 
 export default function Password() {
   const navigate = useNavigate();
-  const [searchParams, setSearchParams] = useSearchParams();
   const dispatch = useDispatch();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [userInfo, setUserInfo] = useState({
+    password: null,
+    newPassword: '',
+  });
+  const [errorData, setErrorData] = useState({
+    type: '',
+    message: '',
+  });
 
   useEffect(() => {
     dispatch(getToken(searchParams.get('token')));
@@ -25,30 +29,66 @@ export default function Password() {
 
   const onClickUpdatePasswordButton = useCallback(async () => {
     try {
-      await updatePassword(data);
+      isCheckProperPassword();
+      await updatePassword(userInfo);
+      setErrorData(null);
       navigate('/');
     } catch (error) {
-      console.log(error.response.data);
+      setErrorData({
+        ...errorData,
+        type: error.message,
+        message: ERROR_MESSAGE[error.message],
+      });
+      // TODO: 인증 실패 예외처리 있으면 적용하기.
+      // if (!error.response) {}
     }
-  }, []);
+  }, [userInfo, errorData]);
+
+  const isCheckProperPassword = () => {
+    const { password, newPassword } = userInfo;
+    if (!validatePassword(password)) {
+      throw new Error('NOT_VALID_FORM');
+    }
+    if (!validatePasswordMatch(password, newPassword)) {
+      throw new Error('NOT_MATCH');
+    }
+  };
 
   return (
     <main className="flex flex-col justify-center h-screen">
       <section>
         <h2 className="text-heading-2 text-center">비밀번호 변경하기</h2>
-        <p className="text-solo-medium text-gray-1 mt-4 text-center">
+        <p className="text-solo-medium text-gray-1 text-center">
           새로운 비밀번호를 입력해주세요
         </p>
       </section>
       <section>
-        <h3 className="mb-3 mt-8">새 비밀번호</h3>
-        <UserInput className="mb-5" placeholder="비밀번호를 입력해주세요" />
-        <h3 className="mb-3 mt-8">새 비밀번호 확인</h3>
-        <UserInput placeholder="비밀번호를 입력해주세요" />
+        <h3>새 비밀번호</h3>
+        <UserInput
+          placeholder="비밀번호를 입력해주세요"
+          type="password"
+          value={userInfo.password || ''}
+          onChange={(e) =>
+            setUserInfo({ ...userInfo, password: e.target.value })
+          }
+          isNotValid={errorData && errorData.type === 'NOT_VALID_FORM'}
+          errorMessage={errorData && errorData.message}
+        />
+        <h3>새 비밀번호 확인</h3>
+        <UserInput
+          placeholder="비밀번호를 입력해주세요"
+          type="password"
+          value={userInfo.newPassword || ''}
+          onChange={(e) =>
+            setUserInfo({ ...userInfo, newPassword: e.target.value })
+          }
+          isNotValid={errorData && errorData.type === 'NOT_MATCH'}
+          errorMessage={errorData && errorData.message}
+        />
         <button
           type="submit"
           onClick={() => onClickUpdatePasswordButton()}
-          className={`${BUTTON_STYLE} bg-orange-400 text-heading-3 text-white mt-8`}
+          className={`${BUTTON_STYLE} bg-orange-400 text-heading-3 text-white py-[22px]`}
         >
           변경하기
         </button>
