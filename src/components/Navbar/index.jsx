803271c213a2e1c597279d3ce1e2a334d502d8a3
Hannah Ahn <ahnlook90@gmail.com>
2023-12-23 22:21:03 +0900

    feat: navbar 뒤로가기 기능 구현

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 import navConfig from './constants';
 import chevronLeft from '../../assets/chevronLeft.svg';
@@ -12,12 +12,19 @@ function Navbar() {
     return null;
   }
 
+  const navigate = useNavigate();
   const { title } = config;
+
+  function handleBack() {
+    navigate(-1);
+  }
 
   return (
     <header className="py-[10px] flex justify-between items-center">
       <section className="flex flex-1 justify-start">
-        <img src={chevronLeft} alt="left" />
+        <button type="button" onClick={handleBack}>
+          <img src={chevronLeft} alt="left" />
+        </button>
       </section>
       <section className="flex-3 text-center text-solo-large">{title}</section>
       <section className="flex flex-1 justify-end" />
