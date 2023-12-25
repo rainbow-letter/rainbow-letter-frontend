3d5fc1dc353d1890e18a10ae178c7001a0017475
Hannah Ahn <ahnlook90@gmail.com>
2023-12-25 21:33:06 +0900

    style: navbar 상단 고정을 위한 스타일

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -22,7 +22,7 @@ function Navbar() {
   }
 
   return (
-    <header className="sticky top-7 py-[10px] flex justify-between items-center">
+    <header className="sticky top-0 pt-[38px] pb-[10px] flex justify-between items-center bg-white">
       <section className="flex flex-1 justify-start">
         <button type="button" onClick={handleBack}>
           <img src={chevronLeft} alt="left" />
