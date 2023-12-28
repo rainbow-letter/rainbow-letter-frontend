df0d7c44c65a61b3faa847812887a5ec265ba5be
Hannah Ahn <ahnlook90@gmail.com>
2023-12-28 16:44:51 +0900

    feat: input을 위한 커스텀훅 작성 및 image input 수정
    - 반려동물 등록 여백 수정
    - input과 관련 alert 컴포넌트를 위한 커스텀훅 작성
    - image input 첨부한 사진 삭제를 위한 스타일
    - 'p-[10px]' 하드코딩을 'p-25'로 수정
    - 폴더 구조 변경

---
+++ src/components/Input/ImageInput/ImageDeleteButton.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function ImageDeleteButton({ icon, onDelete }) {
+  const handleDelete = () => {
+    onDelete();
+  };
+
+  return (
+    <button
+      type="button"
+      className="hidden absolute -top-2.5 -right-2.5"
+      onClick={handleDelete}
+    >
+      <img src={icon} alt="roundX" />
+    </button>
+  );
+}
+
+export default ImageDeleteButton;
--- src/components/PetRegistrationForm/PetRegistrationSection.jsx
+++ src/components/PetRegistrationForm/PetRegistrationSection.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 function PetRegistrationSection({ title, subTitle, children }) {
   return (
-    <section className="mb-4">
-      <div className="py-[10px] solo-medium font-semibold">
+    <section>
+      <div className="py-2.5 solo-medium font-semibold">
         <span>{title}</span>
         <span className="text-gray-2 ml-2">{subTitle}</span>
       </div>
