4b80fdfeb4055471e596fcf1ee66934b69e9881a
Hannah Ahn <ahnlook90@gmail.com>
2023-12-29 12:49:20 +0900

    chore: prop명 수정

--- src/components/InputAlert/index.jsx
+++ src/components/InputAlert/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-function InputAlert({ message, isVisible, isFixed }) {
+function InputAlert({ message, isVisible, reserveSpace = false }) {
   if (!isVisible) {
     return null;
   }
 
-  const styles = isFixed ? 'h-10' : '';
+  const styles = reserveSpace ? 'h-10' : '';
 
   return (
     <div className={`${styles} px-2.5 pt-2.5 text-alarm-red text-caption`}>
