97dac3d49e640e4f43da9703c2048c09c25a1c8e
Hannah Ahn <ahnlook90@gmail.com>
2024-01-06 14:23:18 +0900

    feat: 반려동물 카드 이미지 색션 추가

--- src/components/MyPets/PetCard.jsx
+++ src/components/MyPets/PetCard.jsx
@@ -1,6 +1,9 @@
+/* eslint-disable import/no-cycle */
 import React from 'react';
 
+import { useNavigate } from 'react-router-dom';
 import { calculateDDay } from '../../utils/dateCalculations';
+// import { incrementLikes } from '../../api/favorite';
 import pen from '../../assets/pen.svg';
 import heart from '../../assets/heart.svg';
 
@@ -14,7 +17,7 @@ const dummyPet = {
   image: {
     id: 1,
     objectKey: 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa',
-    url: 'http://rainbowletter/image',
+    url: 'https://gist.github.com/assets/88878874/4a3acbe6-0beb-4dda-8465-70f8d83e61ea',
   },
   favorite: {
     id: 1,
@@ -24,45 +27,74 @@ const dummyPet = {
   },
 };
 
-function PetCard() {
+function PetCard({ pet = dummyPet }) {
+  const navigate = useNavigate();
+
+  const handleEditPet = () => {
+    navigate(`my-pets/${pet.id}/edit`);
+  };
+
+  const handleWriteLetter = () => {
+    navigate(`/letter/write`);
+  };
+
+  const handleFavorite = () => {
+    // incrementLikes(pet.favorite.id);
+    console.log('clicked!');
+  };
+
   return (
-    <article className="mt-1.5 p-4 rounded-2xl shadow-default">
-      <header className="flex justify-between items-center mb-5 mt-2.5 ml-3">
-        <div className="flex items-center grow gap-5">
-          <span className="text-heading-2">{dummyPet.name}</span>
-          <span className="text-solo-small text-orange-400">
-            {calculateDDay(dummyPet.deathAnniversary)}
-          </span>
-        </div>
-        <button className="pr-2" type="button">
-          <img src={pen} alt="pen" />
-        </button>
-      </header>
-      <section className="flex flex-col gap-y-3 mb-6 ml-3 text-solo-medium">
-        <ul className="flex gap-x-1.5">
-          {dummyPet?.personalities.map((personality) => (
-            <li>{`#${personality}`}</li>
-          ))}
-        </ul>
-        <div>{`아이에게 나는 사랑하는 ${dummyPet.owner}`}</div>
-      </section>
-      <footer className="h-[62px] flex gap-x-3 text-body-large">
-        <button
-          className="w-full px-5 py-4 text-white bg-orange-400 rounded-2xl"
-          type="button"
-        >
-          편지 쓰러 가기
-        </button>
-        <button
-          className="flex w-full px-5 py-4 items-center justify-between border text-orange-400 border-orange-400 rounded-2xl"
-          type="button"
-        >
-          <span className="grow">{dummyPet.favorite.total}</span>
-          <div className="p-1.5 border border-orange-100 rounded-full">
-            <img className="" src={heart} alt="heart" />
+    <article className="relative">
+      <figure className="absolute top-0 rounded-2xl">
+        <img
+          className="object-cover rounded-2xl"
+          src={pet.image.url}
+          alt={pet.name}
+          width={354}
+          height={354}
+        />
+      </figure>
+      <div className="absolute top-60 w-full bg-white p-4 rounded-2xl shadow-default">
+        <header className="flex justify-between items-center mb-5 mt-2.5 ml-3">
+          <div className="flex items-center grow gap-5">
+            <span className="text-heading-2">{pet.name}</span>
+            <span className="text-solo-small text-orange-400">
+              {calculateDDay(pet.deathAnniversary)}
+            </span>
           </div>
-        </button>
-      </footer>
+          <button className="pr-2" type="button" onClick={handleEditPet}>
+            <img src={pen} alt="edit" />
+          </button>
+        </header>
+        <section className="flex flex-col gap-y-3 mb-6 ml-3 text-solo-medium">
+          <ul className="flex gap-x-1.5">
+            {pet?.personalities.map((personality) => (
+              // eslint-disable-next-line react/no-array-index-key
+              <li key={personality}>{`#${personality}`}</li>
+            ))}
+          </ul>
+          <div>{`아이에게 나는 사랑하는 ${pet.owner}`}</div>
+        </section>
+        <footer className="h-[62px] flex gap-x-3 text-body-large">
+          <button
+            className="w-full px-5 py-4 flex-1 text-white bg-orange-400 rounded-2xl"
+            type="button"
+            onClick={handleWriteLetter}
+          >
+            편지 쓰러 가기
+          </button>
+          <button
+            className="flex w-full px-5 py-4 flex-1 items-center justify-between border text-orange-400 border-orange-400 rounded-2xl"
+            type="button"
+            onClick={handleFavorite}
+          >
+            <span className="grow">{pet.favorite.total}</span>
+            <div className="p-1.5 border border-orange-100 rounded-full">
+              <img className="active:scale-90" src={heart} alt="heart" />
+            </div>
+          </button>
+        </footer>
+      </div>
     </article>
   );
 }
