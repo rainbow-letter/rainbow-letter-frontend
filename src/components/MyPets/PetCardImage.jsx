bea1eadce33f42c984da2628a1a93c503ef04990
Hannah Ahn <ahnlook90@gmail.com>
2024-01-08 15:03:28 +0900

    style: 반려동물 카드 이미지 사이즈 조정

--- src/components/MyPets/PetCardImage.jsx
+++ src/components/MyPets/PetCardImage.jsx
@@ -4,11 +4,9 @@ function PetCardImage({ name, image }) {
   return (
     <figure className="absolute top-0 bg-gray-2 border border-gary-2 rounded-2xl">
       <img
-        className="object-cover rounded-2xl"
+        className="w-[354px] h-[354px] object-cover rounded-2xl"
         src={image.url}
         alt={name}
-        width={354}
-        height={354}
       />
     </figure>
   );
