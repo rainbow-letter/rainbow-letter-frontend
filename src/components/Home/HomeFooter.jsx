fb56f772ab2088c9bea3df073e496205ee692c3e
Hyuk <poiu03290@gmail.com>
2024-01-16 04:19:02 +0900

    style: footer 태그 변경

--- src/components/Home/HomeFooter.jsx
+++ src/components/Home/HomeFooter.jsx
@@ -5,13 +5,13 @@ import blog from '../../assets/blog.png';
 
 export default function HomeFooter() {
   return (
-    <section className="flex flex-col items-center pt-8 pb-[75px] text-caption">
+    <footer className="flex flex-col items-center pt-8 pb-[75px] text-caption">
       <p className="mb-[18px]">{FOOTER_MESSAGE.ADDRESS}</p>
       <div className="text-center mb-[18px]">
         <p>{FOOTER_MESSAGE.COPYRIGHT}</p>
         <p>{FOOTER_MESSAGE.COPYRIGHT_2}</p>
       </div>
       <img src={blog} width={46} alt="blog" />
-    </section>
+    </footer>
   );
 }
