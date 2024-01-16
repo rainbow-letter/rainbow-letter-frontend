01d4d3ef6468aa3a3a683fc60d5b5543e33cea76
Hannah Ahn <ahnlook90@gmail.com>
2024-01-17 00:14:25 +0900

    feat: admin을 위한 api 함수 생성

--- src/api/reply.js
+++ src/api/reply.js
@@ -8,3 +8,30 @@ export const readReply = async (id) => {
 
   return response;
 };
+
+const RESOURSE_ADMIN = '/api/replies/admin';
+
+export const generateReply = async (id) => {
+  const response = await apiRequest.post(`${RESOURSE_ADMIN}/generate/${id}`);
+
+  return response;
+};
+
+export const inspectReply = async (id) => {
+  const response = await apiRequest.post(`${RESOURSE_ADMIN}/inspect/${id}`);
+
+  return response;
+};
+
+export const submitReply = async (id, reply) => {
+  const response = await apiRequest.post(`${RESOURSE_ADMIN}/${id}`, reply);
+
+  return response;
+};
+
+// reply =
+// {
+//   "letterId" : 1,
+//   "summary" : "답장 최종 제목",
+//   "content" : "답장 최종 본문"
+// }
