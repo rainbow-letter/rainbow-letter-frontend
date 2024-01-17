225dce816e7907078d11f74522e675a3fe5d5790
Hannah Ahn <ahnlook90@gmail.com>
2024-01-17 15:11:29 +0900

    feat: 편지 상세 보기 및 수정 로직 작성

--- src/components/admin/Letters/Editor.jsx
+++ src/components/admin/Letters/Editor.jsx
@@ -1,20 +1,27 @@
+/* eslint-disable import/no-cycle */
 import React from 'react';
 import { createPortal } from 'react-dom';
 
+import { generateReply } from '../../../api/reply';
 import LetterDetailForm from './LetterDetailForm';
 
-function Viewer({ isOpen, content, onClose }) {
+function Editor({ id, isOpen, content, onClose }) {
   if (!isOpen) return null;
+
+  const handleSave = async () => {
+    await generateReply(id);
+    onClose();
+  };
 
   return createPortal(
     <LetterDetailForm
-      mode="view"
       isOpen={isOpen}
       content={content}
       onClose={onClose}
+      onSave={handleSave}
     />,
     document.body
   );
 }
 
-export default Viewer;
+export default Editor;
--- src/components/admin/Letters/LetterDetailForm.jsx
+++ src/components/admin/Letters/LetterDetailForm.jsx
@@ -1,13 +1,37 @@
-import React from 'react';
-import { createPortal } from 'react-dom';
+import React, { useState, useRef } from 'react';
 
-function LetterDetailForm({ isOpen, content = '편지 내용입니다.', onClose }) {
+import useModalClose from '../../../hooks/useModalClose';
+
+function LetterDetailForm({ mode, isOpen, content, onClose, onSave }) {
+  const isViewer = mode === 'view';
+  const modalRef = useRef();
+  const [contentValue, setContentValue] = useState(content);
+
+  useModalClose(modalRef, onClose);
+
   if (!isOpen) return null;
-
-  return createPortal(
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
-      <div className="h-4/5 w-3/5 bg-white p-4 rounded-lg shadow-xl">
-        <div className="flex justify-end mb-4">
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-20">
+      <div
+        className="h-3/5 w-4/5 bg-white p-4 rounded-lg shadow-xl"
+        ref={modalRef}
+      >
+        <textarea
+          className="w-full h-5/6 p-5 mt-4 rounded-lg bg-gray-2 resize-none"
+          disabled={isViewer}
+          value={contentValue}
+          onChange={({ target }) => setContentValue(target.value)}
+        />
+        <div className="flex justify-end mt-3">
+          {isViewer || (
+            <button
+              className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 mr-3 rounded"
+              type="button"
+              onClick={onSave}
+            >
+              save
+            </button>
+          )}
           <button
             className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
             type="button"
@@ -16,10 +40,8 @@ function LetterDetailForm({ isOpen, content = '편지 내용입니다.', onClose
             close
           </button>
         </div>
-        <div className="w-full h-5/6 p-5 rounded-lg bg-gray-2">{content}</div>
       </div>
-    </div>,
-    document.body
+    </div>
   );
 }
 
