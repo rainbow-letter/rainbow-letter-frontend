b089998d097e3b55133e66241d994633b88ff06e
Hyuk <poiu03290@gmail.com>
2024-01-19 14:05:39 +0900

    fix: 홈 푸터 내 링크 삽입

--- src/components/Home/HomeFooter.jsx
+++ src/components/Home/HomeFooter.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import { FOOTER_MESSAGE } from './constants';
 import blog from '../../assets/blog.png';
@@ -6,12 +7,16 @@ import blog from '../../assets/blog.png';
 export default function HomeFooter() {
   return (
     <footer className="flex flex-col items-center pt-8 pb-[75px] text-caption text-gray-1">
-      <p className="mb-[18px]">{FOOTER_MESSAGE.ADDRESS}</p>
+      <Link to={`mailto:${FOOTER_MESSAGE.ADDRESS}`} className="mb-[18px]">
+        {FOOTER_MESSAGE.ADDRESS}
+      </Link>
       <div className="text-center mb-[18px]">
         <p>{FOOTER_MESSAGE.COPYRIGHT}</p>
         <p>{FOOTER_MESSAGE.COPYRIGHT_2}</p>
       </div>
-      <img src={blog} width={46} alt="blog" />
+      <Link to="https://blog.naver.com/rainbowletter" target="_blank">
+        <img src={blog} width={46} alt="blog" />
+      </Link>
     </footer>
   );
 }
