bfa8e63c5689a2ff835deec6c7c71ee2a185ba33
Hyuk <poiu03290@gmail.com>
2024-01-19 14:30:44 +0900

    fix: 로그아웃 사용자가 로그인 화면 접속 시 뒤로가기 수정

--- src/components/Layout/ProtectedLayout.jsx
+++ src/components/Layout/ProtectedLayout.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-cycle */
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 
@@ -6,7 +7,7 @@ import NavBar from '../NavBar';
 
 function ProtectedLayout({ isLoggedIn }) {
   if (!isLoggedIn) {
-    return <Navigate to="/login" />;
+    return <Navigate to="/login" replace />;
   }
 
   return (
