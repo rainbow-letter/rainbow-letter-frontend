68ce19317c28d55882118eb93fe9ee5966bb4700
Hyuk <poiu03290@gmail.com>
2024-01-22 17:17:44 +0900

    fix: 회원가입 페이지 체크박스 라벨 구간 수정

--- src/components/Login/SignUp/CheckBox.jsx
+++ src/components/Login/SignUp/CheckBox.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable  */
 import React from 'react';
 
 import check from '../../../assets/humbleicons_check.svg';
@@ -5,15 +6,24 @@ import check from '../../../assets/humbleicons_check.svg';
 export default function CheckBox({ id, label, onChange, checked }) {
   return (
     <>
-      <input
-        type="checkbox"
-        id={id}
-        onChange={(e) => onChange(e, label)}
-        checked={checked}
-        className="appearance-none w-5 h-5 border border-orange-400 bg-white rounded-[5px] cursor-pointer checked:bg-orange-400"
-      />
-      <img src={check} alt="check" className="absolute w-5 h-5" />
-      <label htmlFor={id} className="font-medium cursor-pointer">
+      <div className="flex">
+        <input
+          type="checkbox"
+          id={id}
+          onChange={(e) => onChange(e, label)}
+          checked={checked}
+          className="appearance-none w-5 h-5 border border-orange-400 bg-white rounded-[5px] cursor-pointer checked:bg-orange-400"
+        />
+        <img
+          src={check}
+          alt="check"
+          onClick={(e) => onChange(e, label)}
+          className={`${
+            checked ? 'block' : 'hidden'
+          } absolute w-5 h-5 cursor-pointer`}
+        />
+      </div>
+      <label htmlFor={id} className="font-medium cursor-pointer w-full">
         {label}
       </label>
     </>
