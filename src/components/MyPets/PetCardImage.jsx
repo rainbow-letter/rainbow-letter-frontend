6062c2479e0f8eef0e7eff3120051a6e05ad841e
Hannah Ahn <ahnlook90@gmail.com>
2024-01-22 21:52:51 +0900

    feat: 등록된 이미지 없을 시 기본 이미지 표시

--- src/components/MyPets/PetCardImage.jsx
+++ src/components/MyPets/PetCardImage.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
+import defaultImage from '../../assets/Logo_256px.png';
+
 function PetCardImage({ name, image }) {
+  const displayedImageUrl = image.url || defaultImage;
+
   return (
     <figure className="w-full h-[354px] absolute inset-x-0 top-0">
       <img
         className="h-full w-full object-cover rounded-2xl border border-gray-2"
-        src={image.url}
+        src={displayedImageUrl}
         alt={name}
       />
     </figure>
