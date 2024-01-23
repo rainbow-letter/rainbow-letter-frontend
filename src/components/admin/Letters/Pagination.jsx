ec6b16ede89b41ed4d94c8baad6c347874e82c08
Hannah Ahn <ahnlook90@gmail.com>
2024-01-23 15:31:13 +0900

    feat: 페이지네이션 버튼 개수 조정 및 맨 앞, 맨 뒤로 가기 구현

--- src/components/admin/Letters/Pagination.jsx
+++ src/components/admin/Letters/Pagination.jsx
@@ -1,17 +1,51 @@
+/* eslint-disable no-lonely-if */
 /* eslint-disable no-plusplus */
 import React from 'react';
 
+import doubleArrowLeft from '../../../assets/admin/doubleArrowLeft.svg';
+import doubleArrowRight from '../../../assets/admin/doubleArrowRight.svg';
+
 function Pagination({ currentPage, totalPages, onPageChange }) {
   const getPaginationNumbers = () => {
-    const pages = [];
-    for (let i = 0; i < totalPages; i++) {
-      pages.push(i);
+    const maxPagesToShow = 10;
+    const halfPagesToShow = Math.floor(maxPagesToShow / 2);
+    let startPage;
+    let endPage;
+
+    if (totalPages <= maxPagesToShow) {
+      startPage = 0;
+      endPage = totalPages - 1;
+    } else {
+      if (currentPage <= halfPagesToShow) {
+        startPage = 0;
+        endPage = maxPagesToShow - 1;
+      } else if (currentPage + halfPagesToShow >= totalPages) {
+        startPage = totalPages - maxPagesToShow;
+        endPage = totalPages - 1;
+      } else {
+        startPage = currentPage - halfPagesToShow;
+        endPage = currentPage + halfPagesToShow;
+      }
     }
-    return pages;
+
+    return Array.from(
+      { length: endPage - startPage + 1 },
+      (_, index) => startPage + index
+    );
   };
 
   return (
     <div className="flex justify-center items-center space-x-2 mt-2.5">
+      {totalPages > 10 && (
+        <button
+          className="w-7 h-7 rounded"
+          type="button"
+          disabled={currentPage === 0}
+          onClick={() => onPageChange(0)}
+        >
+          <img src={doubleArrowLeft} alt="<<" />
+        </button>
+      )}
       {getPaginationNumbers().map((number) => (
         <button
           className={`w-7 h-7 rounded ${
@@ -24,6 +58,16 @@ function Pagination({ currentPage, totalPages, onPageChange }) {
           {number + 1}
         </button>
       ))}
+      {totalPages > 10 && (
+        <button
+          className="w-7 h-7 rounded"
+          type="button"
+          disabled={currentPage === totalPages - 1}
+          onClick={() => onPageChange(totalPages - 1)}
+        >
+          <img src={doubleArrowRight} alt=">>" />
+        </button>
+      )}
     </div>
   );
 }
