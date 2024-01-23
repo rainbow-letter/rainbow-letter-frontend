1ae6a0c511a32756e75e9aa31cd1cb8fe799e6b1
Hyuk <poiu03290@gmail.com>
2024-01-24 03:24:38 +0900

    fix: 편지쓰기 페이지 모달 조건 레이어 수정

--- src/components/Modal/ModalContainer.jsx
+++ src/components/Modal/ModalContainer.jsx
@@ -1,20 +1,8 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
-
-import CancelImage from '../../assets/ph_x-bold.svg';
-import { closeModal } from '../../store/modal';
 
 export default function ModalContainer({ children }) {
-  const dispatch = useDispatch();
   return (
     <article className="bg-white rounded-[15px] relative px-[17px] top-1/3 flex justify-center">
-      <button
-        type="button"
-        onClick={() => dispatch(closeModal())}
-        className="absolute top-4 right-4"
-      >
-        <img src={CancelImage} alt="cancel" />
-      </button>
       {children}
     </article>
   );
