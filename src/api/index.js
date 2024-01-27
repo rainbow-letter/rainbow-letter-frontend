60b7eb0a4ab8a048d24ea44a7d2e71002414ee95
Hannah Ahn <ahnlook90@gmail.com>
2024-01-27 16:07:38 +0900

    feat: api 요청 interceptor에 토큰 유효성 검증 로직

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,11 @@
 /* eslint-disable */
 import axios from 'axios';
 import store from '../index';
+import { removeToken } from '../store/user';
 
 const baseURL = process.env.REACT_APP_API_URL;
 
+const authInstance = axios.create({ baseURL });
 const baseInstance = axios.create({
   baseURL,
   headers: {
@@ -11,16 +13,39 @@ const baseInstance = axios.create({
   },
 });
 
-baseInstance.interceptors.request.use((config) => {
+const checkTokenValidity = async (token) => {
+  try {
+    const response = await authInstance.post(
+      '/api/members/verify',
+      {},
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+    return response.status === 200;
+  } catch (error) {
+    alert('Token validation error:', error);
+    return false;
+  }
+};
+
+baseInstance.interceptors.request.use(async (config) => {
   const { token } = store.getState().user;
   try {
     if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`;
+      const isValid = await checkTokenValidity(token);
+      if (isValid) {
+        config.headers['Authorization'] = `Bearer ${token}`;
+      } else {
+        store.dispatch(removeToken());
+        window.location.href = '/login';
+        return Promise.reject('Invalid token');
+      }
     }
-
     return config;
   } catch (error) {
     console.error(error);
+    return Promise.reject(error);
   }
 });
 
