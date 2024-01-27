ed01dcce4a1c0404720690a7dc6289a656524512
Hannah Ahn <ahnlook90@gmail.com>
2024-01-27 17:19:41 +0900

    feat: 중복 클릭 방지 hook 비동기 로직 추가

--- src/hooks/usePreventDoubleClick.jsx
+++ src/hooks/usePreventDoubleClick.jsx
@@ -1,11 +1,18 @@
+/* eslint-disable no-alert */
 import { useState } from 'react';
 
 const usePreventDoubleClick = () => {
   const [isSubmitting, setIsSubmitting] = useState(false);
 
-  const handleButtonClick = (submitAction) => () => {
+  const handleButtonClick = (submitAction) => async () => {
     setIsSubmitting(true);
-    submitAction();
+    try {
+      await submitAction();
+    } catch (error) {
+      alert(error.message);
+    } finally {
+      setIsSubmitting(false);
+    }
   };
 
   return {
