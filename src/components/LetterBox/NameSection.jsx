2492bc2fed531ba074ada30b8b0b0facb52a0869
Hyuk <poiu03290@gmail.com>
2024-02-01 14:08:51 +0900

    refactor: 편지함 페이지 리팩토링(suspense & spinner)

--- src/components/LetterBox/NameSection.jsx
+++ src/components/LetterBox/NameSection.jsx
@@ -1,18 +1,43 @@
-import React from 'react';
+/* eslint-disable */
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 import Chip from '../Chips/Chip';
+import { getPets } from '../../api/pets';
+import NoPets from '../MyPets/NoPets';
 
-export default function NameSection({ petsNames, onClick, selectedPet }) {
+const DEFAULT = '전체';
+
+export default function NameSection({ onChange, selectedPet }) {
+  const [petsList, setPetsList] = useState(null);
+  const { state } = useLocation();
+
+  useEffect(() => {
+    (async () => {
+      const { pets } = await getPets();
+      const petsNames = pets.map((pet) => pet.name);
+
+      setPetsList([DEFAULT, ...petsNames] || []);
+
+      if (state) {
+        const findedPet = pets.find((pet) => pet.id === state);
+        onChange(findedPet.name || DEFAULT);
+      }
+    })();
+  }, []);
+
+  if (petsList !== null && petsList.length < 1) return <NoPets />;
+
   return (
     <nav>
       <ul className="flex flex-wrap gap-2">
-        {petsNames &&
-          petsNames.map((name) => (
+        {petsList &&
+          petsList.map((name) => (
             <Chip
               key={name}
               value={name}
               isSelected={name === selectedPet}
-              onClick={() => onClick(name)}
+              onClick={() => onChange(name)}
             />
           ))}
       </ul>
