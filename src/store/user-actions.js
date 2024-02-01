ab3c46de8f6027bbd99547e55179e405c6ead6da
Hannah Ahn <ahnlook90@gmail.com>
2024-02-01 09:39:27 +0900

    feat: user slice, thunk 생성

---
+++ src/store/user-actions.js
@@ -0,0 +1,28 @@
+/* eslint-disable import/no-cycle */
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+import api from '../api';
+
+export const fetchUserInfo = createAsyncThunk(
+  'user/fetchUserInfo',
+  async () => {
+    const response = await api.get('/api/members/info');
+    return response;
+  }
+);
+
+export const updatePhoneNumber = createAsyncThunk(
+  'user/updatePhoneNumber',
+  async (phoneNumber) => {
+    const response = await api.put('/api/members/phoneNumber', { phoneNumber });
+    return response;
+  }
+);
+
+export const deletePhoneNumber = createAsyncThunk(
+  'user/deletePhoneNumber',
+  async () => {
+    const response = await api.delete(`/api/members/phoneNumber`);
+    return response;
+  }
+);
