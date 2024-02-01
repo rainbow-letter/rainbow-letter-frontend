86b62f577657d97dbb524bc6bda74eae2e138fb1
Hannah Ahn <ahnlook90@gmail.com>
2024-02-01 11:22:58 +0900

    chore: auth slice name 수정

--- src/store/auth-slice.js
+++ src/store/auth-slice.js
@@ -2,14 +2,11 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const authSlice = createSlice({
-  name: 'user',
-  initialState: { token: null, role: null },
+  name: 'auth',
+  initialState: { token: null },
   reducers: {
     getToken(state, action) {
       state.token = action.payload;
-    },
-    setUserRole(state, action) {
-      state.role = action.payload;
     },
     removeToken(state) {
       state.token = null;
