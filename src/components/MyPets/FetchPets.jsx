cd2352890447eef25f1873ff809e778e09a3617a
Hyuk <poiu03290@gmail.com>
2024-02-01 16:10:10 +0900

    refactor: 아이 정보 페이지 스피너 적용

---
+++ src/components/MyPets/FetchPets.jsx
@@ -0,0 +1,49 @@
+/* eslint-disable no-shadow */
+/* eslint-disable import/no-cycle */
+import React, { useState, useEffect, useRef } from 'react';
+import { useLocation } from 'react-router-dom';
+import { getPets } from '../../api/pets';
+import PetCard from './PetCard';
+import NoPets from './NoPets';
+
+export default function FetchPets() {
+  const { state } = useLocation();
+  const ref = useRef([]);
+  const [pets, setPets] = useState(null);
+
+  const handleGetPets = async () => {
+    const response = await getPets();
+    setPets(response.pets || []);
+  };
+
+  useEffect(() => {
+    handleGetPets();
+  }, []);
+
+  useEffect(() => {
+    if (state) {
+      ref.current[state]?.scrollIntoView({
+        behavior: 'smooth',
+        block: 'center',
+      });
+    }
+  }, [pets]);
+
+  if (pets !== null && pets.length < 1) return <NoPets />;
+  return (
+    <div className="h-full">
+      <ul className="px-1">
+        {pets &&
+          pets.map((pet) => (
+            <PetCard
+              key={pet.id}
+              pet={pet}
+              ref={(el) => {
+                ref.current[pet.id] = el;
+              }}
+            />
+          ))}
+      </ul>
+    </div>
+  );
+}
--- src/components/MyPets/index.jsx
+++ src/components/MyPets/index.jsx
@@ -1,51 +1,20 @@
 /* eslint-disable no-shadow */
 /* eslint-disable import/no-cycle */
-import React, { useState, useEffect, useRef } from 'react';
-import { useLocation } from 'react-router-dom';
-import { getPets } from '../../api/pets';
-import PetCard from './PetCard';
-import NoPets from './NoPets';
-import plus from '../../assets/plus.svg';
+import React, { Suspense } from 'react';
+
+import Spinner from '../Spinner';
 import PetRegisterButton from './PetRegisterButton';
 
+import plus from '../../assets/plus.svg';
+
+const FetchPets = React.lazy(() => import('./FetchPets'));
+
 function MyPets() {
-  const { state } = useLocation();
-  const ref = useRef([]);
-  const [pets, setPets] = useState([]);
-  const existingPets = pets.length > 0;
-
-  const handleGetPets = async () => {
-    const response = await getPets();
-    setPets(response.pets);
-  };
-
-  useEffect(() => {
-    handleGetPets();
-  }, []);
-
-  useEffect(() => {
-    if (state) {
-      ref.current[state]?.scrollIntoView({
-        behavior: 'smooth',
-        block: 'center',
-      });
-    }
-  }, [pets]);
-
-  if (!existingPets) return <NoPets />;
   return (
     <div className="h-full">
-      <ul className="px-1">
-        {pets.map((pet) => (
-          <PetCard
-            key={pet.id}
-            pet={pet}
-            ref={(el) => {
-              ref.current[pet.id] = el;
-            }}
-          />
-        ))}
-      </ul>
+      <Suspense fallback={<Spinner />}>
+        <FetchPets />
+      </Suspense>
       <PetRegisterButton className="flex items-center justify-center gap-x-2 mt-1 py-5 bg-white border border-dashed border-orange-400 rounded-2xl">
         <img src={plus} alt="add" />
         <span className="text-solo-label-pc text-orange-400 font-semibold">
