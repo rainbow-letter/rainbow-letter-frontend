ae088ec95dbf834ab84432b50f0d0ef76159f418
Hannah Ahn <ahnlook90@gmail.com>
2024-02-02 15:35:20 +0900

    feat: redux toolkit의 slice 및 thunk 활용 로직으로 변경

--- src/components/admin/Letters/index.jsx
+++ src/components/admin/Letters/index.jsx
@@ -1,75 +1,21 @@
-/* eslint-disable no-shadow */
-/* eslint-disable import/no-cycle */
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 
-import { loadLetters } from '../../../store/admin/letters';
-import { getLettersForAdmin } from '../../../api/letter';
-import { formatDateToYMD, getPastDate } from '../../../utils/date';
-import { checkLetterStatus } from '../../../utils/replyStatus';
-
-import LetterTable from './LetterTable';
+import { fetchLetters } from '../../../store/admin/letter-actions';
+import LetterTable from './table/LetterTable';
 import Pagination from './Pagination';
-
-const DAYS_AGO = 2;
 
 function Letters() {
   const dispatch = useDispatch();
-  const [currentPage, setCurrentPage] = useState(0);
-  const [totalPages, setTotalPages] = useState(0);
-  const [dateRange, setDateRange] = useState({
-    startDate: getPastDate(DAYS_AGO),
-    endDate: formatDateToYMD(),
-  });
-
-  const setDates = (date) => {
-    setDateRange(date);
-  };
-
-  const getLetters = async () => {
-    const { startDate, endDate } = dateRange;
-    const response = await getLettersForAdmin(startDate, endDate, currentPage);
-
-    const sortedResponse = [...response.content].map((letter) => ({
-      ...letter,
-      reply: {
-        ...letter.reply,
-        status: checkLetterStatus(
-          letter.reply.inspectionTime,
-          letter.reply.timestamp
-        ),
-      },
-    }));
-
-    dispatch(loadLetters(sortedResponse));
-    setTotalPages(response.totalPages);
-  };
-
-  const handlePageChange = (pageNumber) => {
-    setCurrentPage(pageNumber);
-  };
-
-  const handleLetterFilter = () => {
-    setCurrentPage(0);
-    getLetters();
-  };
 
   useEffect(() => {
-    getLetters();
-  }, [dispatch, currentPage]);
+    dispatch(fetchLetters());
+  }, [dispatch]);
 
   return (
     <div className="w-full min-w-[600px] text-solo-label">
-      <LetterTable
-        dateRange={dateRange}
-        onDateSet={setDates}
-        onLetterFilter={handleLetterFilter}
-      />
-      <Pagination
-        currentPage={currentPage}
-        totalPages={totalPages}
-        onPageChange={handlePageChange}
-      />
+      <LetterTable />
+      <Pagination />
     </div>
   );
 }
