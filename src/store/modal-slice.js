359cb6291b3f7d4a6ffba74db85fe6a3dad0cd62
Hannah Ahn <ahnlook90@gmail.com>
2024-02-03 14:39:19 +0900

    feat: redux toolkit 설정, slices 및 actions 정의

---
+++ src/store/modal-slice.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-param-reassign */
+import { createSlice } from '@reduxjs/toolkit';
+
+const modalSlice = createSlice({
+  name: 'modal',
+  initialState: { isOpen: null, type: null, canOpenAgain: true },
+  reducers: {
+    openModal(state, action) {
+      state.isOpen = true;
+      state.type = action.payload;
+    },
+    closeModal(state) {
+      state.isOpen = false;
+      state.type = null;
+    },
+    doNotOpenAgain(state) {
+      state.canOpenAgain = false;
+    },
+  },
+});
+
+export const modalActions = modalSlice.actions;
+export default modalSlice;
---
+++ src/store/user-slice.js
@@ -0,0 +1,44 @@
+/* eslint-disable no-param-reassign */
+import { createSlice } from '@reduxjs/toolkit';
+
+import {
+  fetchUserInfo,
+  updatePhoneNumber,
+  deletePhoneNumber,
+} from './user-actions';
+
+const initialState = {
+  user: { id: null, email: null, phoneNumber: null, role: null },
+  status: 'idle',
+  error: null,
+};
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchUserInfo.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchUserInfo.fulfilled, (state, action) => {
+        state.status = 'success';
+        state.user = action.payload;
+      })
+      .addCase(fetchUserInfo.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      })
+      .addCase(updatePhoneNumber.fulfilled, (state, action) => {
+        state.user.phoneNumber = action.payload.phoneNumber;
+      })
+      .addCase(deletePhoneNumber.fulfilled, (state) => {
+        state.status = 'success';
+        state.user.phoneNumber = null;
+      });
+  },
+});
+
+export const userActions = userSlice.actions;
+export default userSlice;
