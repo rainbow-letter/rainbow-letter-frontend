d00af7fa2a3bf9a6e9ca22660c11948694a5f7c5
Hyuk <poiu03290@gmail.com>
2024-02-09 05:04:24 +0900

    style: 랜딩페이지 gif 파일 mp4 파일로 변경

--- src/components/LandingPage/constants.ts
+++ src/components/LandingPage/constants.ts
@@ -2,8 +2,8 @@ import landing1 from '../../assets/landing-1.svg';
 import landing2 from '../../assets/landing-2.svg';
 import landing3 from '../../assets/landing-3.svg';
 import landing4 from '../../assets/landing-4.svg';
-import writeLetter from '../../assets/wirte-letter.gif';
-import wendHeart from '../../assets/send-heart.gif';
+import writeLetter from '../../assets/write-letter.mp4';
+import wendHeart from '../../assets/send-heart.mp4';
 
 interface LandingItems {
   id: number;
