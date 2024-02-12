82d4dc3792920879e7edd54d151eb112ede6ec19
Hyuk <poiu03290@gmail.com>
2024-02-13 06:27:10 +0900

    feat: PWA 설치 및 세팅

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,22 @@ import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/serviceWorker.js')
+      .then((registration) => {
+        console.log(
+          'Service Worker registered with scope:',
+          registration.scope
+        );
+      })
+      .catch((error) => {
+        console.log('Service Worker registration failed:', error);
+      });
+  });
+}
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
