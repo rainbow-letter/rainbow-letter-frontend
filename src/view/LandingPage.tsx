272c8ff018fdeaed2bcf00f09d2de97385017db8
Hyuk <poiu03290@gmail.com>
2024-02-17 14:29:56 +0900

    style: 랜딩페이지 버튼 위치 변경

--- src/view/LandingPage.tsx
+++ src/view/LandingPage.tsx
@@ -27,7 +27,7 @@ export default function LandingPage() {
           id="service_start"
           disabled={false}
           onClick={onNextPageButtonClick}
-          className="absolute inset-x-0 bottom-16 left-1/2 transform -translate-x-1/2 text-default"
+          className="absolute inset-x-0 bottom-[120px] left-1/2 transform -translate-x-1/2"
         >
           편지 쓰러 가기
         </Button>
