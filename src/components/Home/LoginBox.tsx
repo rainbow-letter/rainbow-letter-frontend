b911f224bf3d17b03beb8d5c8d5f5f1f8fac6313
Hannah Ahn <ahn.hannah.dev@gmail.com>
2024-02-19 17:40:53 +0900

    style: 버튼 그룹 사이즈 통일

--- src/components/Home/LoginBox.tsx
+++ src/components/Home/LoginBox.tsx
@@ -10,11 +10,11 @@ export default function LoginBox() {
         {INFO_MESSAGES.LOGIN_ABSENT}
       </h4>
       <p>{INFO_MESSAGES.INFOMATION_ABOUT_LETTER_WRITING_METHOD}</p>
-      <div className="flex w-full mt-7 mb-4 text-solo-large gap-4">
+      <div className="flex w-full h-[61px] mt-[18px] mb-4 text-solo-large gap-4">
         <Link to="/login" className="w-full">
           <button
             type="button"
-            className="w-full py-5 px-[22px] bg-orange-400 text-white rounded-[15px]"
+            className="w-full h-full py-5 px-[22px] bg-orange-400 text-white rounded-[15px]"
           >
             {USER_ACTIONS.LOGIN}
           </button>
@@ -22,7 +22,7 @@ export default function LoginBox() {
         <Link to="/sign-up" className="w-full">
           <button
             type="button"
-            className="w-full py-5 px-[22px] text-orange-400 border-orange-400 border-2 rounded-[15px]"
+            className="w-full h-full py-5 px-[22px] text-orange-400 border-orange-400 border-2 rounded-[15px]"
           >
             {USER_ACTIONS.SIGH_UP}
           </button>
