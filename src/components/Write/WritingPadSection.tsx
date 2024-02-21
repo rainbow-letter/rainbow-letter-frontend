6db981f13aee67a70ddbee115c2b538fce63f45f
Hannah Ahn <ahn.hannah.dev@gmail.com>
2024-02-21 12:22:08 +0900

    fix: 편지지 밑줄을 rem으로 변경

--- src/components/Write/WritingPadSection.tsx
+++ src/components/Write/WritingPadSection.tsx
@@ -69,7 +69,7 @@ export default function WritingPadSection({
           defaultValue={reply}
           readOnly={!!reply}
           spellCheck="false"
-          className={`${textareaStyle} whitespace-pre-wrap pt-1.5 w-full outline-0 resize-none bg-gradient-to-b from-transparent to-gray-300 from-[97%] to-[3%] bg-[length:1px_48.62px] leading-[187%] text-clip`}
+          className={`${textareaStyle} whitespace-pre-wrap pt-1.5 w-full outline-0 resize-none bg-gradient-to-b from-transparent to-gray-300 from-[97%] to-[3%] bg-[length:1px_3.04rem] leading-[187%] text-clip`}
         />
         <p className="font-sans text-caption text-gray-1 text-right">{date}</p>
       </section>
