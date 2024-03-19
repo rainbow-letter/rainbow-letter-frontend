d9ae2d88d52f4059dad0835623661121e852ac5b
Hyuk <poiu03290@gmail.com>
2024-03-20 01:36:50 +0900

    style: sns 로그인하기 워딩 수정

--- src/components/Login/Header.tsx
+++ src/components/Login/Header.tsx
@@ -4,11 +4,11 @@ import { Message, SNS_LOGIN } from 'components/Login/constants';
 
 type Props = { message: Message };
 
-export default function Header({ message: { title } }: Props) {
+export default function Header({ message: { title, sns } }: Props) {
   return (
     <section>
       <h2 className="text-heading-2">{title}</h2>
-      <h3 className="mt-[2.188rem]">SNS로 간편하게 가입하기</h3>
+      <h3 className="mt-[2.188rem]">{sns}</h3>
       <div className="flex justify-center gap-5 mt-[0.875rem]">
         {SNS_LOGIN.map(({ icon, name, url }) => (
           <a href={url} type="button">
