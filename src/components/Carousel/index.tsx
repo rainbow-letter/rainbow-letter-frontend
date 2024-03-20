d6df4adc3f876a40da6a8ad6321ba3fbacde42eb
Hannah Ahn <ahn.hannah.dev@gmail.com>
2024-03-20 23:43:55 +0900

    feat: 무지개 걸린 편지 캐러셀 적용

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -6,16 +6,18 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 type CarouselProps = {
+  className?: string;
   settings: Record<string, unknown>;
   items: ReactNode[];
 };
 
-function Carousel({ settings, items }: CarouselProps) {
+function Carousel({ className, settings, items }: CarouselProps) {
   const itemsLength = items.length;
+  const style = className || '';
 
   if (!itemsLength) return null;
   return (
-    <Slider {...settings} className="h-10">
+    <Slider {...settings} className={`h-10 ${style}`}>
       {items.map((item) => item)}
     </Slider>
   );
--- src/components/LetterShowcase/index.tsx
+++ src/components/LetterShowcase/index.tsx
@@ -1,31 +1,38 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 
-import useDraggableScroll from 'hooks/useDraggableScroll';
 import LetterItem from 'components/LetterShowcase/LetterItem';
 import { SAMPLE_LETTERS } from 'components/LetterShowcase/constants';
+import Slider from 'react-slick';
 
 function LetterShowcase() {
-  const { containerRef, onMouseDown, onMouseMove, onMouseUpOrLeave } =
-    useDraggableScroll();
+  const carouselSettings = {
+    swipeToSlide: true,
+    arrows: false,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 10000,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    className: 'center',
+    centerMode: true,
+    centerPadding: '30px',
+    speed: 500,
+  };
 
   return (
     <section className="pt-8 pl-5">
-      <span className="py-5 text-solo-large font-bold">
-        무지개에 걸린 편지 한 줄
+      <span className="pt-5 pb-8 text-solo-large font-bold">
+        무지개에 걸린 편지
       </span>
-      <div
-        className="flex py-5 gap-x-2.5 overflow-auto hide-scrollbar"
-        role="presentation"
-        ref={containerRef}
-        onMouseDown={onMouseDown}
-        onMouseMove={onMouseMove}
-        onMouseLeave={onMouseUpOrLeave}
-        onMouseUp={onMouseUpOrLeave}
-      >
+      <div className="h-6" />
+      <Slider {...carouselSettings} className="h-52">
         {SAMPLE_LETTERS.map((letter) => (
-          <LetterItem key={letter.id} letter={letter} />
+          <div className="py-5 w-[138px]">
+            <LetterItem key={letter.id} letter={letter} />
+          </div>
         ))}
-      </div>
+      </Slider>
     </section>
   );
 }
