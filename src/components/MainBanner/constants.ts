95695c8cfe4566f9cf6fc2c9dc403c5af1c39356
ahnlook <ahn.hannah.dev@gmail.com>
2024-03-28 11:29:23 +0900

    feat: 메인 배너 수정

--- src/components/MainBanner/constants.ts
+++ src/components/MainBanner/constants.ts
@@ -1,7 +1,5 @@
 /* eslint-disable import/prefer-default-export */
 import bannerLetter from 'assets/banner-letter.svg';
-import bannerRemembrance from 'assets/banner-remembrance.svg';
-import bannerPetLoss from 'assets/banner-petLoss.svg';
 import bannerCreateLetter from 'assets/banner-createLetter.png';
 
 export const BANNER_ITEMS = [
@@ -14,24 +12,24 @@ export const BANNER_ITEMS = [
     buttonContent: '자세히 보기',
     bgColor: 'bg-[#FFF8ED]',
   },
-  {
-    id: 2,
-    title: '아이를 어떻게\n추모하고 있나요?',
-    description: '여러분의 경험담을\n모으고 있어요',
-    link: 'https://moaform.com/q/ZaL0AH',
-    image: bannerRemembrance,
-    buttonContent: '설문조사 바로가기',
-    bgColor: 'bg-[#F8F8F8]',
-  },
-  {
-    id: 3,
-    title: '나는 펫로스\n증후군일까?',
-    description: '심리상담이 필요한지\n궁금하다면',
-    link: 'https://smore.im/quiz/2re7a9XMt2',
-    image: bannerPetLoss,
-    buttonContent: '테스트하기',
-    bgColor: 'bg-[#FFF1EB]',
-  },
+  // {
+  //   id: 2,
+  //   title: '아이를 어떻게\n추모하고 있나요?',
+  //   description: '여러분의 경험담을\n모으고 있어요',
+  //   link: 'https://moaform.com/q/ZaL0AH',
+  //   image: bannerRemembrance,
+  //   buttonContent: '설문조사 바로가기',
+  //   bgColor: 'bg-[#F8F8F8]',
+  // },
+  // {
+  //   id: 3,
+  //   title: '나는 펫로스\n증후군일까?',
+  //   description: '심리상담이 필요한지\n궁금하다면',
+  //   link: 'https://smore.im/quiz/2re7a9XMt2',
+  //   image: bannerPetLoss,
+  //   buttonContent: '테스트하기',
+  //   bgColor: 'bg-[#FFF1EB]',
+  // },
   {
     id: 4,
     title: '편지를 예쁘게\n만들어드려요',
