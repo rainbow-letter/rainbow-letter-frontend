c61e5e8d4e423a64813a5b9f2d3469ea6121d87c
Hyuk <poiu03290@gmail.com>
2024-03-28 13:29:41 +0900

    style: 홈 쇼케이스 편지 내용 수정

--- src/components/LetterShowcase/constants.ts
+++ src/components/LetterShowcase/constants.ts
@@ -5,64 +5,37 @@ export const SAMPLE_LETTERS: SampleLetter[] = [
   {
     id: '1',
     type: 'letter',
-    petName: '초코',
-    content: `사랑하는 초코야\n형아가 많이 사랑해💕`,
+    petName: '링고',
+    content: `모든 순간 모든 날들이 행복이었고 사랑이었어 정말 너무 사랑해`,
   },
   {
     id: '2',
     type: 'letter',
-    petName: '미니',
-    content:
-      '다시 태어난다면 엄마 곁으로 와줄 수 있니? 못해준 걸 기쁜 맘으로 해줄거야 사랑해💚',
+    petName: '멍코난',
+    content: '너는 내 세계의 중심이었어',
   },
   {
     id: '3',
     type: 'letter',
-    petName: '실버',
-    content: '사랑한다 내\n작은 강아지♥',
+    petName: '루니',
+    content: '사랑하는 내 강아지 너무 보고싶어하는 내 강아지',
   },
   {
     id: '4',
     type: 'reply',
-    petName: '보리',
-    content: '보리는 언니가\n행복하게 웃는 모습을 늘 상상하며\n기다릴거야. ',
+    petName: '복동이',
+    content: '먼 훗날 엄마의 시간도 멈추는 날 다시 만나자 사랑해',
   },
   {
     id: '5',
-    type: 'letter',
-    petName: '토토',
-    content: '엄마가 세상에서\n제일 사랑하는\n사람이야',
+    type: 'reply',
+    petName: '미니',
+    content: '엄마는 항상 내 맘 속에 있어. 엄마가 잘 지내야 나도 행복하거든',
   },
   {
     id: '6',
     type: 'letter',
-    petName: '비앙',
-    content: '비앙아 엄마는 너를 절대 안 잊어. 아니 절대 못 잊어. 평생 사랑해♡',
-  },
-  {
-    id: '7',
-    type: 'reply',
-    petName: '만두',
-    content:
-      '엄마 마음속에서\n만두는 항상 포근하고 따듯해. 만두도 엄마 사랑해, 넘넘 많이!',
-  },
-  {
-    id: '8',
-    type: 'reply',
-    petName: '흰둥이',
-    content: '엄마,바람결에\n귀기울이면 흰둥이의 목소리가 들릴거야',
-  },
-  {
-    id: '9',
-    type: 'reply',
-    petName: '야옹이',
-    content: '사랑한다는 말,\n야옹이 마음속에도 가득 차 있어. 💕',
-  },
-  {
-    id: '10',
-    type: 'reply',
-    petName: '솔이',
-    content:
-      '솔이가 보고 싶으면\n하늘 쳐다보면 돼,\n솔이도 엄마 생각하며 뛰어노니까.',
+    petName: '뚱',
+    content: '매일 더 보고 싶고, 사랑해 우리 뚱',
   },
 ];
