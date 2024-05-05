47204244a293b5f0a17f7c4e28e50ee12011a53b
Hannah Ahn <ahn.hannah.dev@gmail.com>
2024-05-05 14:59:46 +0900

    무지개에 걸린 편지 수정

--- src/components/LetterShowcase/index.tsx
+++ src/components/LetterShowcase/index.tsx
@@ -2,32 +2,32 @@
 import React from 'react';
 
 import LetterItem from 'components/LetterShowcase/LetterItem';
-import { SAMPLE_LETTERS } from 'components/LetterShowcase/constants';
+import { SHOWCASE_LETTERS } from 'components/LetterShowcase/constants';
 import Slider from 'react-slick';
 
-function LetterShowcase() {
-  const carouselSettings = {
-    swipeToSlide: true,
-    arrows: false,
-    infinite: true,
-    autoplay: true,
-    autoplaySpeed: 10000,
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    className: 'center',
-    centerMode: true,
-    centerPadding: '30px',
-    speed: 500,
-  };
+const SHOWCASE_CAROUSEL_OPTIONS = {
+  swipeToSlide: true,
+  arrows: false,
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 10000,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  className: 'center',
+  centerMode: true,
+  centerPadding: '30px',
+  speed: 500,
+} as const;
 
+function LetterShowcase() {
   return (
     <section className="pt-8 pl-5">
       <span className="pt-5 pb-8 text-solo-large font-bold">
         무지개에 걸린 편지
       </span>
       <div className="h-6" />
-      <Slider {...carouselSettings} className="h-52">
-        {SAMPLE_LETTERS.map((letter) => (
+      <Slider {...SHOWCASE_CAROUSEL_OPTIONS} className="h-52">
+        {SHOWCASE_LETTERS.map((letter) => (
           <div className="py-5 w-[138px]">
             <LetterItem key={letter.id} letter={letter} />
           </div>
