92e917edad77a73bd4c89dc7751c849ebc6a58a7
Hyuk <poiu03290@naver.com>
2024-05-26 22:54:58 +0900

    style: 아이폰 전용 저장 이미지 페이지 스타일 수정

--- src/view/letter/SavedImage.tsx
+++ src/view/letter/SavedImage.tsx
@@ -17,8 +17,10 @@ export default function SavedImage() {
 
   return (
     <div className="flex flex-col items-center">
-      <p className="text-heading-3 my-4">편지를 꾹 눌러 저장해보세요!</p>
-      <div>
+      <p className="text-heading-3 mt-4 mb-8">
+        아래 편지를 꾹 눌러 저장해보세요!
+      </p>
+      <div className="border-orange-400 border-2 rounded-xl p-1">
         <img src={savedImageUrl} alt="사진 저장" style={{ maxWidth: '100%' }} />
       </div>
       <Button onClick={onClickGoToHome} className="mt-12">
