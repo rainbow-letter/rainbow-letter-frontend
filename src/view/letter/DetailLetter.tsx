2f7e054a6df011a3f5b13233672c0163aa00df0c
Hyuk <poiu03290@naver.com>
2024-05-26 23:45:49 +0900

    style: 저장 로고 이미지 레이아웃 수정

--- src/view/letter/DetailLetter.tsx
+++ src/view/letter/DetailLetter.tsx
@@ -119,6 +119,9 @@ export default function DetailLetter() {
                 logo.style.bottom = '4px';
                 logo.style.left = '-30px';
                 letterBox.style.paddingBottom = '40px';
+                if (type === 'image_letter') {
+                  letterBox.style.paddingBottom = '50px';
+                }
               }
             }
           },
