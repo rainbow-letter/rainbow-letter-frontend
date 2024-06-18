2843c249be6ad04c79c284fd33d0ebf440803745
Hyuk <poiu03290@naver.com>
2024-06-18 10:23:07 +0900

    feat: getImage 함수 작성

--- src/api/images.ts
+++ src/api/images.ts
@@ -1,7 +1,14 @@
 /* eslint-disable */
 import apiRequest from 'api';
+import { ApiResponse } from 'types/Api';
 
 const RESOURCE = '/api/images';
+
+export const getImage = async (key: string): ApiResponse<string> => {
+  const response = await apiRequest.get(`${RESOURCE}/resources/${key}`);
+
+  return response;
+};
 
 export const updateImageAndGetId = async (data: any): Promise<any> => {
   const response = await apiRequest.post(`${RESOURCE}/upload?type=PET`, data);
