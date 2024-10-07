a2c623fde024d39148ef2a9c3bd0487575c1a058
Hyuk <poiu03290@naver.com>
2024-10-07 21:42:48 +0900

    fix: 아이 이미지 null 일 때, 기본 이미지로 처리

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -1,5 +1,7 @@
+import DefaultImage from 'assets/Logo_256px.png';
+
 export const formatImageType = (data?: string) => {
-  if (!data) return '';
+  if (!data) return DefaultImage;
 
   const baseURL = process.env.REACT_APP_API_URL;
   return `${baseURL}/api/images/resources/${data}`;
