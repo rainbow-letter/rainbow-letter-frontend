a2d15ccbfe08b0ce7c310fe2bc529024440a1b54
ahnlook <hannah.ahn@ringleplus.com>
2024-11-03 16:33:13 +0900

    feat: Add Gemini on/off functionality

--- src/components/MyPageTemplate/AdminLinks.tsx
+++ src/components/MyPageTemplate/AdminLinks.tsx
@@ -1,3 +1,4 @@
+import AdminConfig from 'components/admin/Config';
 import Divider from 'components/Divider';
 import MenuItemLink from 'components/MyPageTemplate/MenuItemLink';
 import { useSelector } from 'react-redux';
@@ -12,6 +13,7 @@ function AdminLinks() {
       <div className="p-2.5 text-heading-3">관리자 페이지</div>
       <div className="mb-[1.375rem] flex flex-col gap-y-[1.375rem]">
         <MenuItemLink to="/admin/letters" label="편지 리스트" />
+        <AdminConfig />
         <Divider />
       </div>
     </>
